9e862a264e734dfd6060737980f1eb5337cd3c11
lh <35501080+luvaihassanali@users.noreply.github.com>
2023-01-09 16:16:01 -0500

    Update module-helper.ps1

--- azure-devops-dynamics-365-pipeline/module-helper.ps1
+++ azure-devops-dynamics-365-pipeline/module-helper.ps1
@@ -1,39 +1,119 @@
-﻿param([string]$url="default",[string]$username="user", [string]$password="pass", [string]$solutionNames="", [string]$onPremises="true", [string]$task="export", [string]$solutionFilePath="path")
+param([string]$url="default",[string]$username="user", [string]$password="pass", [string]$solutionNames="", [string]$onPremises="true", [string]$task="export", [string]$solutionFilePath="path", [string]$solutionOrder="dependencyOrder")
 
-Import-Module -Verbose -FullyQualifiedName "C:\Users\Username\agent-dir\libs\Microsoft.Xrm.Data.Powershell"
+# https://github.com/seanmcne/Microsoft.Xrm.Data.PowerShell/blob/master/Microsoft.Xrm.Data.PowerShell/Microsoft.Xrm.Data.PowerShell.psm1
+Import-Module -FullyQualifiedName "***REMOVED***" # -Verbose
 
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-Set-CrmConnectionTimeout -TimeoutInSeconds 1200
+function Format-XML ([xml]$xml, $indent=2)
+{
+    $StringWriter = New-Object System.IO.StringWriter
+    $XmlWriter = New-Object System.XMl.XmlTextWriter $StringWriter
+    $xmlWriter.Formatting = “indented”
+    $xmlWriter.Indentation = $Indent
+    $xml.WriteContentTo($XmlWriter)
+    $XmlWriter.Flush()
+    $StringWriter.Flush()
+    return $StringWriter.ToString()
+}
 
 if ($onPremises -eq "true") {
-  $crmConnection = Get-CrmConnection -Verbose -ConnectionString "AuthType=Office365;Username=$username;Password=$password;Url=$url" -MaxCrmConnectionTimeOutMinutes 20
+  $crmConnection = Get-CrmConnection -ConnectionString "AuthType=Office365;Username=$username;Password=$password;Url=$url" -MaxCrmConnectionTimeOutMinutes 20 #-Verbose
 } else {
-  $crmConnection = Get-CrmConnection -Verbose -ConnectionString "AuthType=ClientSecret;Url=$url;ClientId=guid;ClientSecret=secret" -MaxCrmConnectionTimeOutMinutes 20
+  $crmConnection = Get-CrmConnection -ConnectionString "AuthType=ClientSecret;Url=$url;ClientId=$clientId;ClientSecret=$clientSecret" -MaxCrmConnectionTimeOutMinutes 20 #-Verbose
 }
-Write-Host "Connection established with TARGET environment:"
-$crmConnection
+Write-Host "Connection established with TARGET environment:" $crmConnection.ConnectedOrgFriendlyName
 
 if ($task -eq "export") {
   $slnNames = "$solutionNames".Split(',')
   Write-Host "Solutions to be exported: $slnNames"
   foreach ($solution in $slnNames) {
+    $exportFlag = $false
     Write-Host "Exporting solution: $solution to $solutionFilePath\solutions"
-    Export-CrmSolution -Verbose -conn $crmConnection -SolutionName $solution -SolutionFilePath "$solutionFilePath\solutions"
+    try {
+      $result = Export-CrmSolution -Verbose -conn $crmConnection -SolutionName $solution -SolutionFilePath "$solutionFilePath\solutions"
+      $exportFlag = $true
+    } 
+    catch {
+        $host.ui.WriteErrorLine("!!! Export-CrmSolution operation has encountered an exception: $_ !!!")
+        exit 1
+    }
   }
+  if (!$exportFlag) {
+    $host.ui.WriteErrorLine("!!! No solutions were exported !!!")
+    exit 1
+  }
+  Write-Host "Completed exporting solutions."
 } 
 
 if ($task -eq "import") {
+  $pollingInterval = 5 #seconds
   $slnNames = Get-ChildItem -Path $solutionFilePath -Name -File -Include *.zip
   $slnOrder = $solutionOrder.Split(',')
   Write-Host "Solutions to be imported: $slnNames"
   foreach($solutionOrderName in $slnOrder) {
+    $publishFlag = $false
     foreach($solution in $slnNames) {
+      $startTime = Get-Date
       if ($solutionOrderName -eq $solution.Split('_')[0]) {
         Write-Host "Importing solution: $solutionFilePath\$solution"
-        Import-CrmSolutionAsync -Verbose -BlockUntilImportComplete -OverwriteUnManagedCustomizations -ActivateWorkflows -conn $crmConnection -SolutionFilePath "$solutionFilePath\$solution" -MaxWaitTimeInSeconds 1200    
+        try {
+          $startTime = Get-Date
+          $asyncId = Import-CrmSolutionAsync -OverwriteUnManagedCustomizations -ActivateWorkflows -conn $crmConnection -SolutionFilePath "$solutionFilePath\$solution" #-MaxWaitTimeInSeconds 1200 -BlockUntilImportComplete -Verbose
+
+          Write-Host "Querying async job ID:" $asyncId.AsyncJobId
+          while ($true) {
+            # https://github.com/seanmcne/Microsoft.Xrm.Data.PowerShell.Samples/issues/23
+            try {
+              $asyncResult = Get-CrmRecord -conn $crmConnection -EntityLogicalName asyncoperation -Id $asyncId.AsyncJobId -Fields * 
+            } 
+            catch {
+              $host.ui.WriteErrorLine("!!! Get-CrmRecord operation has encountered an exception: $_ !!!")
+              exit 1
+            }
+
+            $friendlyMsg = $asyncResult.friendlymessage
+            $completedOn = $asyncResult.completedon
+            $stateCode = $asyncResult.statecode
+            $stopTime = Get-Date
+            $timeSpan = $stopTime - $startTime
+            
+            Write-Host "Elapsed time:"$timeSpan.ToString()
+            Write-Host "State code:" $stateCode
+            Write-Host "Completed on:" $completedOn
+            Write-Host "Friendly message:" $friendlyMsg
+            Write-Host "-"
+
+            if ($friendlyMsg -and $friendlyMsg.Contains("FAILURE:")) {
+              $fMArr = $friendlyMsg -split '<', 2
+              $xmlcontent = "<" + ($fMArr[1] -split ',', -1)[0].Trim()
+              $xml = New-Object -TypeName System.Xml.XmlDocument
+              $xml.LoadXml($xmlcontent)
+              $host.ui.WriteErrorLine("!!! Solution import failed !!!`n" + (Format-Xml $xml))
+              exit 1
+            }
+            
+            if ($completedOn) {          
+              $publishFlag = $true
+              break;
+            }
+
+            if ($timeSpan.TotalMinutes -gt 20) {
+              $host.ui.WriteErrorLine("!!! Solution exceeded timeout !!!")
+              exit 1
+            }
+            Start-Sleep -s $pollingInterval
+          }
+        } 
+        catch {
+          $host.ui.WriteErrorLine("!!! Import-CrmSolutionAsync operation has encountered an exception: $_ !!!")
+          exit 1
+        }
       }
     }
   }
-  Write-Host "Publishing all customizations..."
-  Publish-CrmAllCustomization -Verbose -conn $crmConnection
+  if ($publish) {
+    Write-Host "Publishing all customizations..."
+    Publish-CrmAllCustomization -Verbose -conn $crmConnection
+  }
 }
+
+exit 0
