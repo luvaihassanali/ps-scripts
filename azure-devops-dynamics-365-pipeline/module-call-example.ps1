9c2a9f42f4ab7f56633708e5a5d81fe5eafc4f5c
lh <35501080+luvaihassanali@users.noreply.github.com>
2023-02-22 15:37:34 -0500

    Update module-call-example.ps1

--- azure-devops-dynamics-365-pipeline/module-call-example.ps1
+++ azure-devops-dynamics-365-pipeline/module-call-example.ps1
@@ -1,8 +1,9 @@
-﻿$isOnPrem= "false"
-$import = "false"
-$url = "url" 
-$url2 = "https://env.crm.dynamics.com/"#
-$path = "C:\Users\Username\agent-dir\libs"
+$isOnPrem= "false"
+$import = "true"
+$url = "" 
+$url2 = ""
+$path = "$modulePath"
+$localpath = "C:\Users\Username\Desktop\temp"
 
 $psi = New-object System.Diagnostics.ProcessStartInfo
 $psi.CreateNoWindow = $true
@@ -18,20 +19,21 @@ if ($isOnPrem -eq "true") {
   $pw = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($adminPassword))
 
   if ($import -eq "true") {
-    $psi.Arguments = "-File `"C:\Users\Username\agent-dir\libs\module-helper.ps1`" -url `"$url`" -onPremises `"true`" -task `"import`" -solutionFilePath `"${PSScriptRoot}`" -username `"email`" -password `"$pw`" -Wait"
+    $psi.Arguments = "-File `"$path\module-helper.ps1`" -url `"$url`" -onPremises `"true`" -task `"import`" -solutionFilePath `"$localpath`" -username `"<insert username>`" -password `"$pw`" -Wait"
   } else {
-    $psi.Arguments = "-File `"C:\Users\Username\agent-dir\libs\module-helper.ps1`" -url `"$url`" -onPremises `"true`" -task `"export`" -solutionName `"Solution1,Solution2`" -solutionFilePath `"${PSScriptRoot}`" -username `"email`" -password `"$pw`" -Wait"
+    $psi.Arguments = "-File `"$path\module-helper.ps1`" -url `"$url`" -onPremises `"true`" -task `"export`" -solutionName `"Solution1,Solution2`" -solutionFilePath `"$localpath`" -username `"<insert username>`" -password `"$pw`" -Wait"
   }
 } else { # Cloud test 
   if ($import -eq "true") {
-    $psi.Arguments = "-File `"$path\module-helper.ps1`" -url `"$url2`" -onPremises `"false`" -task `"import`" -solutionFilePath `"${PSScriptRoot}`" -Wait"
+    $psi.Arguments = "-File `"$path\module-helper.ps1`" -url `"$url2`" -onPremises `"false`" -task `"import`" -solutionFilePath `"$localpath`" -solutionOrder `"Solution1,Solution2`" -Wait"
   } else {
-    $psi.Arguments = "-File `"$path\module-helper.ps1`" -url `"$url2`" -onPremises `"false`" -task `"export`" -solutionName `"Solution1,Solution2`" -solutionFilePath `"${PSScriptRoot}`" -Wait"
+    $psi.Arguments = "-File `"$path\module-helper.ps1`" -url `"$url2`" -onPremises `"false`" -task `"export`" -solutionName `"Solution1,Solution2`" -solutionFilePath `"$localpath`" -Wait"
   }
 }
+
 Write-Host $psi.Arguments
-$process = New-Object System.Diagnostics.Process 
-$process.StartInfo = $psi 
+$process = New-Object System.Diagnostics.Process
+$process.StartInfo = $psi
 [void]$process.Start()
 
 do
